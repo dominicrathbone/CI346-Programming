5c8e2a881484f73f4ad811b73dcc49cb65532000
Dominic Rathbone <drathbone@localhost.localdomain>
2015-11-23 14:03:55 +0000

    removed runner interface

--- assignment/ConcurrencyTester/src/com/company/ExplicitConcurrencyRunner.java
+++ assignment/ConcurrencyTester/src/com/company/ExplicitConcurrencyRunner.java
@@ -3,7 +3,10 @@ package com.company;
 /**
  * Created by drathbone on 22/11/15.
  */
-public class ExplicitConcurrencyRunner implements Runner {
+public class ExplicitConcurrencyRunner implements Runnable {
+
+    public ExplicitConcurrencyRunner() {
+    }
 
     public void run() {
 
--- assignment/ConcurrencyTester/src/com/company/ImplicitConcurrencyRunner.java
+++ assignment/ConcurrencyTester/src/com/company/ImplicitConcurrencyRunner.java
@@ -3,7 +3,10 @@ package com.company;
 /**
  * Created by drathbone on 22/11/15.
  */
-public class ImplicitConcurrencyRunner implements Runner {
+public class ImplicitConcurrencyRunner implements Runnable{
+
+    public ImplicitConcurrencyRunner() {
+    }
 
     public void run() {
 
