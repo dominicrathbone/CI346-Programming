309b8d05f775fe4a5ccf6d3c7a07f12161c53848
Dominic Rathbone <drathbone@ln-mowifi-02-10.1.30.111.net.bton.ac.uk>
2015-11-27 15:55:51 +0000

    updated report

--- assignment/ConcurrencyTester/src/com/company/ImplicitConcurrencyRunner.java
+++ assignment/ConcurrencyTester/src/com/company/ImplicitConcurrencyRunner.java
@@ -3,7 +3,7 @@ package com.company;
 /**
  * Created by drathbone on 22/11/15.
  */
-public class ImplicitConcurrencyRunner implements Runnable{
+public class ImplicitConcurrencyRunner{
 
     public ImplicitConcurrencyRunner() {
     }
--- assignment/ConcurrencyTester/src/com/company/Main.java
+++ assignment/ConcurrencyTester/src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         NonConcurrentRunner nonConcurrentRunner = new NonConcurrentRunner();
         ImplicitConcurrencyRunner implicitConcurrencyRunner = new ImplicitConcurrencyRunner();
         ExplicitConcurrencyRunner explicitConcurrencyRunner = new ExplicitConcurrencyRunner();
@@ -12,19 +13,17 @@ public class Main {
         nonConcurrentRunner.run();
         long nonConcurrentEndTime = System.nanoTime();
 
-        long nonConcurrentTotalTimeRun = nonConcurrentEndTime - nonConcurrentStartTime;
-
         long implicitStartTime = System.nanoTime();
         implicitConcurrencyRunner.run();
         long implicitEndTime = System.nanoTime();
-
-        long implicitTotalTimeRun = implicitEndTime - implicitStartTime;
 
         long explicitStartTime = System.nanoTime();
         Thread thread = new Thread(explicitConcurrencyRunner);
         thread.start();
         long explicitEndTime = System.nanoTime();
 
+        long nonConcurrentTotalTimeRun = nonConcurrentEndTime - nonConcurrentStartTime;
+        long implicitTotalTimeRun = implicitEndTime - implicitStartTime;
         long explicitTotalTimeRun = explicitEndTime - explicitStartTime;
 
         System.out.println("non concurrent run took " + nonConcurrentTotalTimeRun + " seconds");
